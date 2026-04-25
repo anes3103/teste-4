a8c4320d9d172f402f6578a691ef31368af66d19
anes3103 <maduu.3103@gmail.com>
2026-04-25 01:21:27 -0300

    git e funções

--- equipe.js
+++ equipe.js
@@ -1,15 +1,15 @@
 const membros = [
             { nome: "Maria Eduarda", cargo: "Design do Site", github: "anes3103", email: "m.anes@uni9.edu.br", img: "imagens/avatar/Eduarda.jpeg"},     
-            { nome: "Felipe", cargo: "Testes e Integração", github: "felipe-dev", email: "yfelipeferreirasilvay@uni9.edu.br", img: "imagens/avatar/Felipe.jpeg" },
-            { nome: "Karen", cargo: "Gestão e Revisão Final", github: "karen-design", email: "karen.freman.nascimento@uni9.edu.br ", img: "imagens/avatar/Karen.jpeg" },
-            { nome: "Wesley", cargo: "Analista de banco de dados", github: "wexsouza", email: "wesley.souza@uni9.edu.br", img: "imagens/avatar/Wesley.jpeg" },
-            { nome: "Ighor", cargo: "Lógica de Mapas e Conexão", github: "ighorieckes-alt", email: "ighor.ieckes@uni9.edu.br", img: "imagens/avatar/Ighor.jpeg" },
-            { nome: "Julia", cargo: "Design do Mascote e Roteiro", github: "JuliaTuratti", email: "julia.c.turatti@uni9.edu.br", img: "imagens/avatar/Julia.jpeg" },
-            { nome: "Luana", cargo: "Edição de Vídeo e Mídia", github: "luana-sec", email: " lc.alencar@uni9.edu.br", img: "imagens/avatar/Luana.jpeg" },
-            { nome: "Miguel", cargo: "Finalização e Qualidade", github: "miguel-mob", email: " lucasnunes.borges@uni9.edu.br", img: "imagens/avatar/Miguel.jpeg" },
-            { nome: "Laryssa", cargo: "Montagem do Circuito", github: "lary-manager", email: " laryssa.freitas@uni9.edu.br ", img: "imagens/avatar/Laryssa.jpeg" },
-            { nome: "Nicolas", cargo: "Programação do Arduino", github: "nicolas-qa", email: "ferraz.nicolas@uni9.edu.br", img: "imagens/avatar/Nicolas.jpeg" },
-            { nome: "Marco", cargo: "Orçamentos e Hardware", github: "marco-arch", email: "ferreira.marco@uni9.edu.br", img: "imagens/avatar/Marco.jpeg" },    
+            { nome: "Felipe", cargo: "Testes e Integração", github: "Ferreira-18", email: "yfelipeferreirasilvay@uni9.edu.br", img: "imagens/avatar/Felipe.jpeg" },
+            { nome: "Karen", cargo: "Gestão e Revisão Final", github: "Fremaan3110", email: "karen.freman.nascimento@uni9.edu.br ", img: "imagens/avatar/Karen.jpeg" },
+            { nome: "Wesley", cargo: "Lógica de Mapas e conexão", github: "wexsouza", email: "wesley.souza@uni9.edu.br", img: "imagens/avatar/Wesley.jpeg" },
+            { nome: "Ighor", cargo: "Arquitetura de Software", github: "ighorieckes-alt", email: "ighor.ieckes@uni9.edu.br", img: "imagens/avatar/Ighor.jpeg" },
+            { nome: "Julia", cargo: "Edição de vídeo e mídia", github: "JuliaTuratti", email: "julia.c.turatti@uni9.edu.br", img: "imagens/avatar/Julia.jpeg" },
+            { nome: "Luana", cargo: "Design do Mascote e Roteiro", github: "LuanaAlencar0", email: " lc.alencar@uni9.edu.br", img: "imagens/avatar/Luana.jpeg" },
+            { nome: "Miguel", cargo: "Finalização, Qualidade e ABNT", github: "lucasnunesborges", email: " lucasnunes.borges@uni9.edu.br", img: "imagens/avatar/Miguel.jpeg" },
+            { nome: "Laryssa", cargo: "Programação do Arduino", github: "laryssafreitas", email: " laryssa.freitas@uni9.edu.br ", img: "imagens/avatar/Laryssa.jpeg" },
+            { nome: "Nicolas", cargo: "Montagem do circuito", github: "Nferraz10", email: "ferraz.nicolas@uni9.edu.br", img: "imagens/avatar/Nicolas.jpeg" },
+            { nome: "Marco", cargo: "Orçamentos e Hardware", github: "marcogfer98-hash", email: "ferreira.marco@uni9.edu.br", img: "imagens/avatar/Marco.jpeg" },    
     ];
 
         const container = document.getElementById('team-container');
