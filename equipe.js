d001b8f1de0af2eee2bbb425f9ad2b2e09409d92
wesley <wesley.dnx@gmail.com>
2026-04-25 20:15:28 -0300

    simplificação no JS - equipe, que ficou mais simples. Outra mudança foi nos github que estão rodando agora

--- equipe.js
+++ equipe.js
@@ -5,31 +5,32 @@ const membros = [
             { nome: "Wesley", cargo: "Lógica de dados e Maps e Conexão", github: "wexsouza", email: "wesley.souza@uni9.edu.br", img: "imagens/avatar/Wesley.jpeg" },
             { nome: "Ighor", cargo: "Arquitetura do Software", github: "ighorieckes-alt", email: "ighor.ieckes@uni9.edu.br", img: "imagens/avatar/Ighor.jpeg" },
             { nome: "Julia", cargo: "Edição de Vídeo e Mídia", github: "julia-sys", email: "julia.c.turatti@uni9.edu.br", img: "imagens/avatar/Julia.jpeg" },
-            { nome: "Luana", cargo: "Design do Mascote e Roteiro ", github: "luana-sec", email: " lc.alencar@uni9.edu.br", img: "imagens/avatar/Luana.jpeg" },
-            { nome: "Miguel", cargo: "Finalização, qualidade e ABNT", github: "miguel-mob", email: " lucasnunes.borges@uni9.edu.br", img: "imagens/avatar/Miguel.jpeg" },
-            { nome: "Laryssa", cargo: "Programação do Arduino", github: "lary-manager", email: " laryssa.freitas@uni9.edu.br ", img: "imagens/avatar/Laryssa.jpeg" },
-            { nome: "Nicolas", cargo: "Montagem do Circuito", github: "nicolas-qa", email: "ferraz.nicolas@uni9.edu.br", img: "imagens/avatar/Nicolas.jpeg" },
+            { nome: "Luana", cargo: "Design do Mascote e Roteiro ", github: "LuanaAlencar0", email: " lc.alencar@uni9.edu.br", img: "imagens/avatar/Luana.jpeg" },
+            { nome: "Miguel", cargo: "Finalização, qualidade e ABNT", github: "lucasnunesbordes", email: " lucasnunes.borges@uni9.edu.br", img: "imagens/avatar/Miguel.jpeg" },
+            { nome: "Laryssa", cargo: "Programação do Arduino", github: "laryssafreitas", email: " laryssa.freitas@uni9.edu.br ", img: "imagens/avatar/Laryssa.jpeg" },
+            { nome: "Nicolas", cargo: "Montagem do Circuito", github: "Nferraz10", email: "ferraznicolas2102@gmail.com", img: "imagens/avatar/Nicolas.jpeg" },
             { nome: "Marco", cargo: "Orçamentos e Hardware", github: "marco-arch", email: "ferreira.marco@uni9.edu.br", img: "imagens/avatar/Marco.jpeg" },    
     ];
 
-        const container = document.getElementById('team-container');
+const container = document.getElementById('team-container');
+let html = '';
 
+membros.forEach(m => {  
+    html += `
+        <div class="member-small glass-card">
+            <img src="${m.img}" alt="${m.nome}">
+            <h4>${m.nome}</h4>
+            <span class="role-tag">${m.cargo}</span>
+            <div class="member-links">
+                <a href="https://github.com/${m.github}" target="_blank" title="GitHub">
+                    <i class="fab fa-github"></i>
+                </a>
+                <a href="mailto:${m.email}" title="E-mail">
+                    <i class="fas fa-envelope"></i>
+                </a>
+            </div>
+        </div>
+    `;
+});
 
-        membros.forEach(m => {  
-            container.innerHTML += `
-                <div class="member-small glass-card">
-                    <img src="${m.img}" alt="${m.nome}">
-                    <h4>${m.nome}</h4>
-                    <span class="role-tag">${m.cargo}</span>
-                    
-                    <div class="member-links">
-                        <a href="https://github.com/${m.github}" target="_blank" title="GitHub">
-                            <i class="fab fa-github"></i>
-                        </a>
-                        <a href="mailto:${m.email}" title="E-mail">
-                            <i class="fas fa-envelope"></i>
-                        </a>
-                    </div>
-                </div>
-            `;
-        });
+container.innerHTML = html;
